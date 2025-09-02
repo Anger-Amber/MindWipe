72385053d708326cdd9d3e520df312cb88a18096
Anton Vahlenberg <anton.vahlenberg@learnet.se>
2025-09-02 14:27:31 +0200

    Movement copied and polishing of existing movement

---
+++ Assets/Scripts/Tontis Scripts/TempCamera.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class TempCamera : MonoBehaviour
+{
+    [SerializeField] Transform Player;
+    [SerializeField] float cameraSpeed;
+
+    Vector3 cameraPosition;    
+
+    void Update()
+    {
+        cameraPosition = Vector2.Lerp(Player.position, transform.position, Mathf.Pow(0.5f, cameraSpeed * Time.deltaTime));
+        cameraPosition.z = -10;
+        transform.position = cameraPosition;
+    }
+}
